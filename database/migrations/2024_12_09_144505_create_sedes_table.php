85c20601e8a92b325c295d5153cfbd3a94be1191
ManuelGallego1 <manuela.gallegoa@autonoma.edu.co>
2024-12-09 15:09:34 -0500

    Pint (linter)

--- database/migrations/2024_12_09_144505_create_sedes_table.php
+++ database/migrations/2024_12_09_144505_create_sedes_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -15,8 +16,7 @@ return new class extends Migration {
             $table->string('nombre');
             $table->unsignedBigInteger('coordinador_id');
             $table->boolean('activo');
-            $table->foreign('coordinador_id')->references('id')
-                ->on('users');
+            $table->foreign('coordinador_id')->references('id')->on('users');
         });
     }
 
--- database/migrations/2024_12_09_144805_create_movil_table.php
+++ database/migrations/2024_12_09_144805_create_movil_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -16,7 +17,11 @@ return new class extends Migration {
             $table->string('min', 10);
             $table->string('imei', 15);
             $table->string('iccid', 17);
-            $table->enum('tipo', ['kit prepago', 'kit financiado', 'wb', 'up grade', 'linea nueva', 'reposicion', 'portabilidad pre', 'portabilidad pos', 'venta de tecnologia', 'equipo pos']);
+            $table->enum('tipo', [
+                'kit prepago', 'kit financiado', 'wb', 'up grade',
+                'linea nueva', 'reposicion', 'portabilidad pre',
+                'portabilidad pos', 'venta de tecnologia', 'equipo pos',
+            ]);
             $table->unsignedBigInteger('plan_id');
             $table->unsignedBigInteger('celulares_id');
             $table->string('cliente_cc');
@@ -30,7 +35,7 @@ return new class extends Migration {
             $table->enum('financiera', ['crediminuto', 'celya', 'brilla', 'N/A']);
             $table->unsignedBigInteger('coordinador_id');
             $table->enum('estado', ['pendiente', 'exitosa', 'rechazada', 'cancelada', 'terminada']);
-            
+
             $table->foreign('plan_id')->references('id')->on('planes')->onDelete('cascade');
             $table->foreign('celulares_id')->references('id')->on('celulares')->onDelete('cascade');
             $table->foreign('coordinador_id')->references('id')->on('users')->onDelete('cascade');
