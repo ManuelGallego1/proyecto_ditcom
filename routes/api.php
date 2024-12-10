6f80dbfff50663499c620a02465e7c57e93aff35
Samuel05L28 <samuelescobaralvarez@gmail.com>
2024-12-09 21:57:12 -0500

    rutas de controladores

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\CelularesController;
+use App\Http\Controllers\Api\ClientesController;
+use App\Http\Controllers\Api\PlanesController;
+use App\Http\Controllers\Api\SedeController;
+use App\Http\Controllers\Api\SedeVendedorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +16,13 @@ Route::post('/v1/login', [App\Http\Controllers\api\AuthController::class, 'login
 
 Route::post('/v1/register', [App\Http\Controllers\api\AuthController::class, 'register'])->name('api.register');
 
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/v1/logout', [App\Http\Controllers\api\AuthController::class, 'logout'])->name('api.logout');
+    Route::apiResource('sede-vendedores', SedeVendedorController::class);
+    Route::apiResource('celulares', CelularesController::class);
+    Route::apiResource('clientes', ClientesController::class);
+    Route::apiResource('planes', PlanesController::class);
+    Route::apiResource('sede', SedeController::class);
+
 });
